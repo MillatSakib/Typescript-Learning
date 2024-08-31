e32c5aef2b8a39d5339b8e5198f3dff75c890f2a
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-08-31 11:14:04 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var userName = "anisul islam";
+console.log(userName);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+addNumbers(20, 30);
+//index.ts -> tsc.index.ts --watch -> nodemon index.js
---
+++ index.ts
@@ -0,0 +1,11 @@
+let userName: string = "anisul islam";
+console.log(userName);
+
+function addNumbers(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+
+addNumbers(20, 30);
+addNumbers(20, 30);
+
+//index.ts -> tsc.index.ts --watch -> nodemon index.js
